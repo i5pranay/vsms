89eb464cdbb7c0a975beda3440cde191be4cffcf
pranay.singh <just2pranay@hotmail.com>
2017-02-13 22:26:17 +0530

    changed role to super_admin for heruku

--- db/migrate/20160712134713_add_role_to_users.rb
+++ db/migrate/20160712134713_add_role_to_users.rb
@@ -1,5 +1,5 @@
 class AddRoleToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :role, :string, default: :customer, null: false, limit: 40
+    add_column :users, :role, :string, default: :super_admin, null: false, limit: 40
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -101,9 +101,9 @@ ActiveRecord::Schema.define(version: 20160728183919) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "encrypted_password",     limit: 255,                        null: false
-    t.string   "name",                   limit: 100,                        null: false
-    t.string   "email",                  limit: 200,                        null: false
+    t.string   "encrypted_password",     limit: 255,                           null: false
+    t.string   "name",                   limit: 100,                           null: false
+    t.string   "email",                  limit: 200,                           null: false
     t.text     "address",                limit: 65535
     t.string   "city",                   limit: 255
     t.string   "state",                  limit: 255
@@ -112,14 +112,14 @@ ActiveRecord::Schema.define(version: 20160728183919) do
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,     default: 0,          null: false
+    t.integer  "sign_in_count",          limit: 4,     default: 0,             null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                                null: false
-    t.datetime "updated_at",                                                null: false
-    t.string   "role",                   limit: 40,    default: "customer", null: false
+    t.datetime "created_at",                                                   null: false
+    t.datetime "updated_at",                                                   null: false
+    t.string   "role",                   limit: 40,    default: "super_admin", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
